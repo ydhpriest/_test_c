e2b96dee2bb4e22b16b378b5a3bdf4e26e8e8275
ydhpriest <81673978+ydhpriest@users.noreply.github.com>
2021-03-31 17:05:41 +0900

    テスト_プッシュ_20210331

---
+++ _test_hello_20210331_c/src/_test_hello_20210331_c.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : _test_hello_20210331_c.c
+ Author      : Yang Yohan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ _test_hi_20210331_c/src/_test_hi_20210331_c.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : _test_hi_20210331_c.c
+ Author      : Yang Yohan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hi World!!!"); /* prints !!!Hi World!!! */
+	return EXIT_SUCCESS;
+}
